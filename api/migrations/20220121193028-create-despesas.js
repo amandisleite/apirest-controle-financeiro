b37e16d6f9710b270d6711e8f2a13508562a81c3
Amanda Leite <mandirleite@gmail.com>
2022-01-21 16:35:03 -0300

    model de Receitas e Despesas criados

---
+++ api/migrations/20220121193028-create-despesas.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Despesas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      descricao: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      valor: {
+        allowNull: false,
+        type: Sequelize.FLOAT
+      },
+      data: {
+        allowNull: false,
+        type: Sequelize.DATEONLY
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Despesas');
+  }
+};
