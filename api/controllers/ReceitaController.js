3da0f1972ee55e613179e167f1d8fc827def0f78
Amanda Leite <mandirleite@gmail.com>
2022-01-21 19:57:27 -0300

    tentativa de impedimento de cadastro de mesma descrição no mesmo mês feita

--- api/controllers/ReceitaController.js
+++ api/controllers/ReceitaController.js
@@ -1,27 +1,46 @@
 const database = require("../models");
+const moment = require("moment");
 const { Op } = require("sequelize");
+const sequelize = require("sequelize");
 
 class Receitas {
     static async cadastroDeReceita(req, res) {
         const novaReceita = req.body;
-        const descricaoNovaReceita = req.body.descricao;
         const dataNovaReceita = req.body.data;
-        let dataConvertida = new Date(dataNovaReceita.split('/').reverse().join('/'));
+
+        const descricaoNovaReceita = req.body.descricao;
+        let mesNovaReceita = (moment(dataNovaReceita).month()) + 1;
+        let anoNovaReceita = moment(dataNovaReceita).year();
+
+        let anoTodasReceitas = await database.Receitas.findAll({attributes: [[ sequelize.fn('YEAR', sequelize.col('data')), 'ano']] })
+        let mesTodasReceitas = await database.Receitas.findAll({attributes: [[ sequelize.fn('MONTH', sequelize.col('data')), 'mes']] })
         
+        let arrayComTodosMeses = [];
+        let arrayComTodosAnos = [];
+
+        for (let i = 0; i < mesTodasReceitas.length; i++) {
+            let mes = Object.values(mesTodasReceitas[i].dataValues)
+            arrayComTodosMeses.push(Number(mes))
+        }
+
+        for (let i = 0; i < anoTodasReceitas.length; i++) {
+            let ano = Object.values(anoTodasReceitas[i].dataValues)
+            arrayComTodosAnos.push(Number(ano))
+        }
+
+        let mesmoAnoDaRequisicao = arrayComTodosAnos.filter((a, ano) => arrayComTodosAnos[ano] === anoNovaReceita)
+        let mesmoMesDaRequisicao = arrayComTodosMeses.filter((a, mes) => arrayComTodosMeses[mes] === mesNovaReceita)
+
         try {
-            const descricaoEmTodasReceitas = await database.Receitas.findOne({
+            const descricaoEmTodasReceitas = await database.Receitas.findAll({
                 where: {
-                    [Op.and]: {
-                        descricao: descricaoNovaReceita,
-                        data: {
-                                [Op.lte]: dataNovaReceita,
-                                [Op.gte]: new Date(dataConvertida - 30 * 24 * 60 * 60 * 1000)
-                        }
-                    }
+                    descricao: descricaoNovaReceita,
                 }
             });
-    
-            if (descricaoEmTodasReceitas) {
+
+            if (descricaoEmTodasReceitas &&
+                mesmoAnoDaRequisicao.length > 0 &&
+                mesmoMesDaRequisicao.length > 0) {
                 throw new Error('receita com a mesma descrição já cadastrada nesse mês')
             } else {
                 try {
@@ -34,25 +53,6 @@ class Receitas {
         } catch (error) {
             return res.status(400).json(error.message)
         }
-        // const camposParaAtualizar = {};
-
-        // camposRequisicao.forEach( campo => {
-        //     const valor = this[campo];
-        //     const camposDeTodasReceitas = database.Receitas.findAll({ where: { descricao: valor }});
-        //     // if (valor !== await database.Receitas.findAll({ where: { descricao: valor }})) {
-        //         //     camposParaAtualizar[campo] = valor;
-        //         // }
-        //         if (valor === camposDeTodasReceitas) {
-        //             throw new Error('receita já cadastrada nesse mês')
-        //         } else {
-        //             try {
-        //                 const receitaCriada = database.Receitas.create(novaReceita);
-        //                 return res.status(200).json(receitaCriada);
-        //             } catch (error) {
-        //                 return res.status(400).json(error.message)
-        //             }
-        //         }
-        //     })
     }
 
     static async listagemDeReceitas(req, res) {
