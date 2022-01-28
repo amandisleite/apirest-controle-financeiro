1f4357e4c669cb1a8dba444c0c67caf31d8b4e53
Amanda Leite <mandirleite@gmail.com>
2022-01-28 10:17:06 -0300

    ajuste nas migrations antes de rodá-las

--- api/migrations/20220128100242-addcolumn-despesas.js
+++ api/migrations/20220128100242-addcolumn-despesas.js
@@ -1,22 +1,15 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Receitas', {
-      categorias: {
-        allowNull: true,
-        type: Sequelize.ENUM({
-          values: ['Alimentação', 'Saúde', 'Moradia', 'Transporte', 'Educação', 'Lazer', 'Imprevistos', 'Outras']
+    await queryInterface.addColumn('Despesas', 'categorias', {
+      allowNull: true,
+      type: Sequelize.ENUM({
+        values: ['Alimentação', 'Saúde', 'Moradia', 'Transporte', 'Educação', 'Lazer', 'Imprevistos', 'Outras']
         }),
-        defaultValue: 'Outras'
-      },
-      deletedAt: {
-        allowNull: true,
-        type: Sequelize.DATE
-      }
-
+      defaultValue: 'Outras'
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Receitas', 'deletedAT');
+    await queryInterface.removeColumn('Despesas', 'categorias');
   }
 };
